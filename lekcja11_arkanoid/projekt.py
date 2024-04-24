f5bc7f153a380c4f93254c6e1197e56959dab0f3
Krystian Piszczela <01168907@pw.edu.pl>
2024-04-24 18:57:36 +0200

    wstep do arkanoida

--- lekcja11_arkanoid/projekt.py
+++ lekcja11_arkanoid/projekt.py
@@ -6,4 +6,23 @@ WYSOKOSC_EKRANU = 800
 pygame.init()
 ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
 zegar = pygame.time.Clock()
-obraz_tla = pygame.image.load('images/background.png')
+obraz_tla = pygame.image.load('lekcja11_arkanoid/images/background.png')
+
+gra_dziala = True
+while gra_dziala:
+    for zdarzenie in pygame.event.get():
+        if zdarzenie.type == pygame.KEYDOWN:
+            if zdarzenie.key == pygame.K_ESCAPE:
+                gra_dziala = False
+        elif zdarzenie.type == pygame.QUIT:
+            gra_dziala = False
+
+    # sterowanie platformą
+
+    # rysowanie tła
+    ekran.blit(obraz_tla, (0, 0))
+    pygame.display.flip()
+    zegar.tick(30)
+
+# dodanie 3 sekund opoźnienia po przegraniu
+pygame.quit()
