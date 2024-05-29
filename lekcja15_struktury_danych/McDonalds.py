e142b294d0495cd07c5e24f0d9503dafb0e83937
Krystian Piszczela <01168907@pw.edu.pl>
2024-05-29 20:06:21 +0200

    kolejka

--- lekcja15_struktury_danych/McDonalds.py
+++ lekcja15_struktury_danych/McDonalds.py
@@ -12,13 +12,32 @@ class McDQueue:
         self.queue = Queue()
 
     def is_empty(self):
-        pass
+        return self.queue.is_empty()
 
-    def add_customer(name, order):
-        pass
+    def add_customer(self, name, order):
+        customer = Customer(name, order)
+        self.queue.enqueue(customer)
 
     def remove_customer(self):
-        pass
+        if not self.is_empty():
+            return self.queue.dequeue()
 
     def next_customer_order(self):
-        pass
+        if not self.is_empty():
+            next_customer = self.queue.peek()
+            return next_customer.order
+
+queue = McDQueue()
+
+queue.add_customer('Krystian', 'hamburger')
+queue.add_customer('Mati', 'frytki')
+queue.add_customer('Ola', 'cola')
+queue.add_customer('Marysia', 'cheeseburger')
+
+while not queue.is_empty():
+    next_customer = queue.remove_customer()
+    print(f'Obsługujemy klienta {next_customer.name}, który zamówił {next_customer.order}')
+
+    if not queue.is_empty():
+        next_order = queue.next_customer_order()
+        print(f'Nastepny klient zamowił {next_order}\n\n')
