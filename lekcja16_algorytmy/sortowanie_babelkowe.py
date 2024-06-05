b986f426ac80bacc7c57afa943219c9ef10a9ee9
Krystian Piszczela <01168907@pw.edu.pl>
2024-06-05 19:16:12 +0200

    sortowanie babelkowe

---
+++ lekcja16_algorytmy/sortowanie_babelkowe.py
@@ -0,0 +1,22 @@
+import random
+
+my_list = []
+
+for i in range(2000):
+    my_list.append(random.randint(1, 100))  # losowa liczba z zakresu 1-100
+
+
+def bubble_sort(my_list):
+    n = len(my_list)
+    licznik_krokow = 0
+    for j in range(n):
+        for i in range(n-1):
+            licznik_krokow += 1
+            if my_list[i] > my_list[i+1]:
+                my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+    print(licznik_krokow)
+    return my_list
+
+# print(my_list)
+sorted_list = bubble_sort(my_list)
+# print(sorted_list)
