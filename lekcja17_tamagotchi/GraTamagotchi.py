af728272d248f772341e9dc306eff648a2df7b25
Krystian Piszczela <01168907@pw.edu.pl>
2024-06-12 19:43:12 +0200

    dodane guziki

--- lekcja17_tamagotchi/GraTamagotchi.py
+++ lekcja17_tamagotchi/GraTamagotchi.py
@@ -32,6 +32,14 @@ CZCIONKA = pygame.font.SysFont("Comic Sans", 20)
 # Inicjalizjacja Tamagotchi
 tamagotchi = Tamagotchi()
 
+# Funkcja pomocnicza
+def okresl_kolor(poziom):
+    if poziom <= 30:
+        return KOLOR_CZERWONY
+    if 30 < poziom <= 70:
+        return KOLOR_ZOLTY
+    return KOLOR_ZIELONY
+
 # Główna pętla gry
 gra_dziala = True
 while gra_dziala:
@@ -46,15 +54,35 @@ while gra_dziala:
     ekran.fill(kolor_tla)
     # Interfejs
     ## pasek szczescia
-    szerokosc_paska_szczescie = tamagotchi.poziom_szczescia
-    pygame.draw.rect(ekran, KOLOR_CZARNY, pygame.Rect(50, 50, szerokosc_paska_szczescie, 20))
+    szerokosc_paska_szczescie = 3*tamagotchi.poziom_szczescia
+    kolor_szczescie = okresl_kolor(tamagotchi.poziom_szczescia)
+    pygame.draw.rect(ekran, kolor_szczescie, pygame.Rect(50, 50, szerokosc_paska_szczescie, 20))
     tekst_szczescie = CZCIONKA.render("Szczęście:", True, KOLOR_CZARNY)
     ekran.blit(tekst_szczescie, (50, 20))
+
     ## pasek głodu
-    szerokosc_paska_glodu = tamagotchi.poziom_glodu
-    pygame.draw.rect(ekran, KOLOR_CZARNY, pygame.Rect(50, 100, szerokosc_paska_glodu, 20))
+    szerokosc_paska_glodu = 3*tamagotchi.poziom_glodu
+    kolor_glod = okresl_kolor(tamagotchi.poziom_glodu)
+    pygame.draw.rect(ekran, kolor_glod, pygame.Rect(50, 100, szerokosc_paska_glodu, 20))
     tekst_glod = CZCIONKA.render("Poziom głodu:", True, KOLOR_CZARNY)
     ekran.blit(tekst_glod, (50, 70))
+
+    ## przyciski Nakarm i Pobaw się
+    przycisk_nakarm = pygame.Rect(25, 300, 150, 50)
+    przycisk_pobaw = pygame.Rect(225, 300, 150, 50)
+    pygame.draw.rect(ekran, KOLOR_BIALY, przycisk_nakarm, 3)
+    pygame.draw.rect(ekran, KOLOR_BIALY, przycisk_pobaw, 3)
+
+    tekst_nakarm = CZCIONKA.render("Nakarm", True, KOLOR_CZARNY)
+    tekst_pobaw = CZCIONKA.render("Pobaw się", True, KOLOR_CZARNY)
+
+    szer_tekst_nakarm, wys_tekst_nakarm = tekst_nakarm.get_size()
+    szer_tekst_pobaw, wys_tekst_pobaw = tekst_pobaw.get_size()
+
+    ekran.blit(tekst_nakarm, (przycisk_nakarm.x + (przycisk_nakarm.width - szer_tekst_nakarm)//2, 310))
+    ekran.blit(tekst_pobaw, (przycisk_pobaw.x + (przycisk_pobaw.width - szer_tekst_pobaw)//2, 310))
+
+
 
     """ ! ruszamy dopiero pod koniec zajęć !
     nastroj = tamagotchi.poziom_glodu + tamagotchi.poziom_szczescia
